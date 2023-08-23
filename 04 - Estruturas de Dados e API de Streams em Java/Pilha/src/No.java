7deebfb1fed4a6a650969690f89089fd7c8f98f8
gabriel-leone <leonepgabriel@gmail.com>
2023-08-23 13:11:28 -0300

    Aula sobre Pilhas

---
+++ 04 - Estruturas de Dados e API de Streams em Java/Pilha/src/No.java
@@ -0,0 +1,28 @@
+public class No {
+  private int dado;
+  private No reNo = null;
+
+  public No(int dado) {
+    this.dado = dado;
+  }
+
+  public int getDado() {
+    return dado;
+  }
+
+  public void setDado(int dado) {
+    this.dado = dado;
+  }
+
+  public void setRefNo(No reNo) {
+    this.reNo = reNo;
+  }
+
+  public No getRefNo() {
+    return reNo;
+  }
+
+  public String toString() {
+    return "No{" + "dado=" + dado + '}';
+  }
+}
