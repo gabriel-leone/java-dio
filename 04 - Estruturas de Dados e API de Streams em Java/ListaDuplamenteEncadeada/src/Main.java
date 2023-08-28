14dcfbd3283c60e2db8560fd917c6ba23bde64a4
gabriel-leone <leonepgabriel@gmail.com>
2023-08-28 19:31:33 -0300

    Aula de Listas Duplamente Encadeadas

---
+++ 04 - Estruturas de Dados e API de Streams em Java/ListaDuplamenteEncadeada/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+  public static void main(String[] args) {
+    ListaDuplamenteEncadeada<String> minhaListaEncadeada = new ListaDuplamenteEncadeada<>();
+
+    minhaListaEncadeada.add("Rafael");
+    minhaListaEncadeada.add("Carlos");
+    minhaListaEncadeada.add("Joao");
+    minhaListaEncadeada.add("Maria");
+    minhaListaEncadeada.add("Jose");
+    minhaListaEncadeada.add("Antonio");
+
+    System.out.println(minhaListaEncadeada);
+
+    minhaListaEncadeada.remove(3);
+
+    System.out.println(minhaListaEncadeada);
+
+    minhaListaEncadeada.add(3, "Larissa");
+
+    System.out.println(minhaListaEncadeada);
+    System.out.println(minhaListaEncadeada.get(3));
+  }
+}
